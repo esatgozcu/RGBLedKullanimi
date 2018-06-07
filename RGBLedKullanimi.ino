44ac4d1f304861704e5f334c99c52bbc71fe3efe
esatgozcu <35576161+esatgozcu@users.noreply.github.com>
2018-06-07 05:54:37 +0300

    Add files via upload

---
+++ RGBLedKullanimi.ino
@@ -0,0 +1,27 @@
+const int kirmizi = 6;
+const int yesil = 5;
+const int mavi = 3;
+
+void setup() {
+
+  pinMode(kirmizi,OUTPUT);
+  pinMode(yesil,OUTPUT);
+  pinMode(mavi,OUTPUT);
+
+  analogWrite(kirmizi,255);
+  analogWrite(yesil,255);
+  analogWrite(mavi,255);
+
+  /*
+      Ortak Anotlu RGB Led Kullananlar için
+
+  analogWrite(kirmizi,255- istediğimiz değer);
+  analogWrite(yesil,255- istediğimiz değer);
+  analogWrite(mavi,255- istediğimiz değer);
+   
+  */
+}
+
+void loop() {
+
+}
